8c023856a6d5479c8adc7b23ff5a55048568c1b5
Эдуард Птушко <ptushko@gmail.com>
2023-12-18 19:32:06 +0300

    Initial Commit

---
+++ ToDoApp/ToDoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoAppApp.swift
+//  ToDoApp
+//
+//  Created by Eduard Ptushko on 18.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
